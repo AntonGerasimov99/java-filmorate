5219c309428460c8710197315af4794d952cd7c5
Anton <GerasimovAnton347@yandex.ru>
2023-05-28 20:15:00 +0500

    ФЗ9 Исправлены замечания

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Film {
+
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Duration duration;
+}
